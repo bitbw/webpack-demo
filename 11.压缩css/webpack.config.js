66c15bf4bbfa5ff7f7945d3a7a002e84bb343a3e
张博文 <18211132604@163.com>
2021-02-19 17:59:41 +0800

    update

---
+++ 11.压缩css/webpack.config.js
@@ -0,0 +1,85 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// webpack v4 使用 optimize-css-assets-webpack-plugin
+// 对于webpack v5或更高版本，请改用css-minimizer-webpack-plugin。
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+process.env.NODE_ENV = 'development'
+module.exports = {
+  target: process.env.NODE_ENV == 'development' ? 'web' : 'browserslist',
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: resolve(__dirname, "dist")
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          /*
+            css兼容性处理：postcss --> postcss-loader -postcsspreset-env
+
+            帮postcss找到package.json中browserslist里面的配置，通过配置加载指定的css兼容性样式
+
+            "browserslist": {
+              // 开发环境 --> 设置node环境变量：process.env.NODE_ENV = development
+              "development": [
+                "last 1 chrome version",
+                "last 1 firefox version",
+                "last 1 safari version"
+              ],
+              // 生产环境：默认是看生产环境
+              "production": [
+                ">0.2%",
+                "not dead",
+                "not op_mini all"
+              ]
+            }
+          */
+          // 使用loader的默认配置
+          // 'postcss-loader',
+          // 修改loader的配置
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
+                  ],
+                  // 或者 [require("postcss-preset-env")()]
+                ]
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    // 对输出的css文件进行重命名
+    new MiniCssExtractPlugin({
+      filename: "css/index.css"
+    }),
+    new CleanWebpackPlugin()
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
